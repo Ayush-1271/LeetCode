14dca49a1af7a27d7c3d0ba159e5caa831c752b1
Ayush Ranjan <87318600+Ayush-1271@users.noreply.github.com>
2026-09-18 01:35:58 +0530

    Time: 15 ms (20.01%) | Memory: 21.5 MB (5.57%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,6 +1,5 @@
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
-        def find():
             n = 0
             for i, v in enumerate(nums):
                 if n ==0:
@@ -12,11 +11,3 @@ class Solution:
                     else:
                         n -= 1
             return m
-        
-        c, t = 0, find()
-        for i in nums:
-            if i == t:
-                c += 1
-
-        if c > len(nums)//2:
-            return t
