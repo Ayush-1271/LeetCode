8bb18a92d0f8f23af5156ff8d727d5182ec1d0a3
Ayush Ranjan <87318600+Ayush-1271@users.noreply.github.com>
2026-09-20 09:58:05 +0530

    Time: 6 ms (76.61%) | Memory: 19.3 MB (55.57%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -2,6 +2,6 @@ class Solution:
     def reverseDegree(self, s: str) -> int:
         res = 0
         for i in range(len(s)):
-            res += (ord('{') - ord(s[i])) * (i+1)
+            res += (123 - ord(s[i])) * (i+1)
 
         return res
