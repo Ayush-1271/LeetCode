8005c3d1d3dfae311619368b484d121dbb6bbb87
Ayush Ranjan <87318600+Ayush-1271@users.noreply.github.com>
2026-09-25 17:12:42 +0530

    Time: 4 ms (34.43%) | Memory: 19.6 MB (29.51%) - LeetSync

---
+++ 1188-brace-expansion-ii/brace-expansion-ii.py
@@ -0,0 +1,38 @@
+class Solution:
+    def braceExpansionII(self, expression: str) -> list[str]:
+        op, stk = [], []
+
+        def ope():
+            l, r = len(stk) - 2, len(stk) - 1
+            if op[-1]=='+':
+                stk[l] |= stk[r]
+            else:
+                t = set()
+                for i in stk[l]:
+                    for j in stk[r]:
+                        t.add(i+j)
+                stk[l] = t
+            op.pop()
+            stk.pop() 
+
+        for i, ch in enumerate(expression):
+            if ch == ',':
+                while op and op[-1] == '*':
+                    ope()
+                op.append("+")
+            elif ch == '{':
+                if i>0 and (expression[i-1]=='}' or expression[i-1].isalpha()):
+                    op.append('*')
+                op.append('{')
+            elif ch == '}':
+                while op and op[-1] != '{':
+                    ope()
+                op.pop()
+            else:
+                if i>0 and (expression[i-1] == '}' or expression[i-1].isalpha()):
+                    op.append("*")
+                stk.append({ch})
+        while op:
+            ope()
+        
+        return sorted(list(stk[-1]))
